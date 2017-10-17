8b8b320ca5434cf544882ba84c57347d88aaef87
johnnpeacock <johnnpeacock@users.noreply.github.com>
2017-10-17 11:05:39 -0400

    Update batch_download_genbank.pl

--- batch_download_genbank.pl
+++ batch_download_genbank.pl
@@ -1,9 +1,7 @@
 #!/usr/bin/perl
 #############################################
-#Author: Jiang Li
-#email:  riverlee2008@gmail.com
-#Creat Time: Tue 23 Oct 2012 01:37:54 PM CDT
-#Vanderbilt Center for Quantitative Sciences
+#Author: John Peacock
+#email:  johnnpeacock@gmail.com
 #############################################
 use strict;
 use warnings;
@@ -27,7 +25,7 @@ In your current working directory. There will generate 20 ChrXX.gb files.
 
 
 ## Script begin
-# The chromosomes ids of cat comes from http://www.ncbi.nlm.nih.gov/genome/78
+# The chromosomes ids of Mus musculus comes from http://www.ncbi.nlm.nih.gov/genome/52
 # Apply NCIB eutils to download genbank (full) format files of each chromosome
 
 my $log="download.log";
@@ -35,7 +33,7 @@ while(<DATA>){
     next if (!/^Chr/);
     my ($chr,$name,$refseq,@other) = split /\s+/;
     print "[",scalar(localtime),"] Downloading ${chr}${name}|$refseq \n";
-    my $comm = "wget \"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=$refseq&rettype=gbwithparts&retmode=text\" -O ${chr}${name}.gb >>$log 2>&1";
+    my $comm = "wget \"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=$refseq&rettype=gbwithparts&retmode=text\" -O ${chr}${name}.gb >>$log 2>&1 --no-check-certificate";
     
     #print $comm,"\n\n";
     
@@ -48,24 +46,27 @@ while(<DATA>){
 }
 
 __DATA__
-Type	 Name	 RefSeq	 INSDC	 Size (Mb)	 GC%	 Protein	 rRNA	 tRNA	 Other RNA	 Gene	 Pseudogene
-Chr	A1	NC_018723.1	CM001378.1	239.3	0.0	1,398	-	18	22	1,491	285
-Chr	A2	NC_018724.1	CM001379.1	169.04	0.0	1,746	-	28	11	1,736	203
-Chr	A3	NC_018725.1	CM001380.1	142.46	0.0	1,310	-	11	16	1,235	148
-Chr	B1	NC_018726.1	CM001381.1	205.24	0.0	1,099	-	8	24	1,210	295
-Chr	B2	NC_018727.1	CM001382.1	154.26	0.0	1,161	-	153	17	1,423	268
-Chr	B3	NC_018728.1	CM001383.1	148.49	0.0	1,355	-	33	12	1,421	219
-Chr	B4	NC_018729.1	CM001384.1	144.26	0.0	1,420	-	13	15	1,402	241
-Chr	C1	NC_018730.1	CM001385.1	221.44	0.0	1,957	-	41	31	1,957	292
-Chr	C2	NC_018731.1	CM001386.1	157.66	0.0	1,000	-	11	16	1,076	234
-Chr	D1	NC_018732.1	CM001387.1	116.87	0.0	1,583	-	16	14	1,558	222
-Chr	D2	NC_018733.1	CM001388.1	89.82	0.0	729	-	3	10	688	94
-Chr	D3	NC_018734.1	CM001389.1	95.74	0.0	767	-	16	12	814	121
-Chr	D4	NC_018735.1	CM001390.1	96.02	0.0	862	-	8	14	868	135
-Chr	E1	NC_018736.1	CM001391.1	63	0.0	1,234	-	43	5	1,145	67
-Chr	E2	NC_018737.1	CM001392.1	64.04	0.0	1,104	-	14	9	1,141	139
-Chr	E3	NC_018738.1	CM001393.1	43.02	0.0	716	-	24	6	699	37
-Chr	F1	NC_018739.1	CM001394.1	68.67	0.0	738	-	20	3	730	81
-Chr	F2	NC_018740.1	CM001395.1	82.76	0.0	455	-	11	8	509	112
-Chr	X	NC_018741.1	CM001396.1	126.43	0.0	865	-	19	29	963	270
-Chr	MT	NC_001700.1	U20753.1	0.017009	40.3	13	2	22	-	13	-
+Type	Name	RefSeq	INSDC	Size (Mb)	GC%	Protein	rRNA	tRNA	Other RNA	Gene	Pseudogene
+Chr	1	NC_000067.6	CM000994.2	195.47	41.3	4,731	-	37	2,031	2,687	579
+Chr	2	NC_000068.7	CM000995.2	182.11	42.2	6,282	-	8	2,783	3,491	609
+Chr	3	NC_000069.6	CM000996.2	160.04	40.7	3,507	-	40	1,663	2,225	480
+Chr	4	NC_000070.6	CM000997.2	156.51	42.5	4,710	-	8	1,991	2,622	497
+Chr	5	NC_000071.6	CM000998.2	151.84	42.7	4,634	-	10	1,952	2,507	413
+Chr	6	NC_000072.6	CM000999.2	149.74	41.6	3,844	1	53	1,572	2,597	555
+Chr	7	NC_000073.6	CM001000.2	145.44	43.2	6,336	-	10	2,087	3,798	935
+Chr	8	NC_000074.6	CM001001.2	129.4	42.6	3,653	32	7	1,673	2,177	376
+Chr	9	NC_000075.6	CM001002.2	124.6	42.9	4,406	-	7	1,698	2,276	374
+Chr	10	NC_000076.6	CM001003.2	130.7	41.6	3,546	-	11	1,546	2,086	391
+Chr	11	NC_000077.6	CM001004.2	122.08	44.0	5,650	-	47	2,097	2,852	381
+Chr	12	NC_000078.6	CM001005.2	120.13	42.0	2,621	-	3	1,593	2,002	516
+Chr	13	NC_000079.6	CM001006.2	120.42	41.9	2,536	-	105	1,513	2,127	476
+Chr	14	NC_000080.6	CM001007.2	124.9	41.4	3,007	-	12	1,538	2,111	455
+Chr	15	NC_000081.6	CM001008.2	104.04	42.2	2,872	-	4	1,349	1,620	282
+Chr	16	NC_000082.6	CM001009.2	98.21	41.2	2,486	-	2	1,081	1,367	257
+Chr	17	NC_000083.6	CM001010.2	94.99	42.9	3,610	-	12	1,373	2,005	427
+Chr	18	NC_000084.6	CM001011.2	90.7	41.7	1,855	-	-	966	1,218	264
+Chr	19	NC_000085.6	CM001012.2	61.43	43.1	2,328	-	10	831	1,283	205
+Chr	X	NC_000086.7	CM001013.2	171.03	39.7	3,010	-	17	933	2,291	912
+Chr	Y	NC_000087.7	CM001014.2	91.74	39.3	335	-	-	119	423	85
+MT	NC_005089.1	AY172335.1	0.02	36.7	13	2	22	-	37	-
+Un	-	.	-	93.44	43.6	2,471	2	10	1,126	2,899	501
